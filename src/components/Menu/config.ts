979437f5048225392abdde267f31e54b17ab130e
SiaCashCoin <39414933+SiaCashCoin@users.noreply.github.com>
2022-04-19 22:48:31 -0400

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -60,7 +60,7 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
       },
       {
         label: 'CoinFlip',
-        href: 'href:https://tokenpvp.com/tokens/0xc26EaFC627624baDf990f8d30116892eD204DB51',
+        href: 'https://tokenpvp.com/tokens/0xc26EaFC627624baDf990f8d30116892eD204DB51',
       },
     ],
   },
